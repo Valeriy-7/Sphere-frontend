a3649483f132758eafc93e0e81e36824544956f3
Bivekich <biveki-dev@yandex.ru>
2025-03-23 20:53:30 +0300

    Обновлен адрес API в окружении разработки и добавлена поддержка передачи куки для CORS запросов в axios-клиенте.

--- modules/auth/axios-client.ts
+++ modules/auth/axios-client.ts
@@ -5,8 +5,9 @@ export type { RequestConfig, ResponseErrorConfig } from '@kubb/plugin-client/cli
 //client.setConfig({baseURL:'http://188.225.18.179:3000'})
 
 //axiosInstance.defaults.baseURL = 'http://188.225.18.179:3000';
-console.log(process.env.NEXT_PUBLIC_API);
+console.log('API URL:', process.env.NEXT_PUBLIC_API);
 axiosInstance.defaults.baseURL = process.env.NEXT_PUBLIC_API;
+axiosInstance.defaults.withCredentials = true; // Включаем передачу куки для CORS запросов
 
 export default client;
 
